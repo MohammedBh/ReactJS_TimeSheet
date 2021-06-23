4adb37a4875255762a53da544cdb7110e2f03d8a
Roxuno <80962586+Roxuno@users.noreply.github.com>
2021-06-23 09:16:11 +0200

    Header + Footer

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Modal from './components/Modal/Modal'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Modal></Modal>
+        <Header></Header>
+        <Footer></Footer>
       </div>
     )
   }
---
+++ project/src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css'
+
+class Footer extends React.Component {
+
+    render() {
+
+        return(
+            <footer>
+                <h3>
+                    Copyright by Ouss & Moh & Iss & Yas
+                </h3>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
