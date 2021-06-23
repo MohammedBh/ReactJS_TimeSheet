4fbafa82d7702a016a54605e1612d215709bebe8
Roxuno <80962586+Roxuno@users.noreply.github.com>
2021-06-24 01:56:46 +0200

    TimeSheet ReactJS

--- project/src/components/Body/Body.js
+++ project/src/components/Body/Body.js
@@ -1,7 +1,6 @@
 import React from "react"
 import 'bootstrap/dist/js/bootstrap.min.js';
 import "./Body.css"
-import Navigation from "../Navigation/Navigation"
 import Clock from "../clock/Clock";
 import Table from "../Table/Table";
 import MyModal from "../Modal/Modal";
